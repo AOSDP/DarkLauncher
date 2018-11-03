648b3f1ed3a87191f23c41e81e0bae834c1ef7c0
YAJATapps <yajatapps3@gmail.com>
2018-11-03 18:55:05 +0530

    Fix a bug where pageindicator was struck and not hiding some times

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -428,6 +428,8 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
         mDragInfo = null;
         mOutlineProvider = null;
         mDragSourceInternal = null;
+
+        getPageIndicator().setShouldAutoHide(true);
     }
 
     /**
